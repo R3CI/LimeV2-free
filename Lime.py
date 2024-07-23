16f8f524dad7f7c507e1d3fc69590cfcd1eb1d8c
r3ci <139361332+R3CI@users.noreply.github.com>
2024-07-23 12:50:28 +0200

    Create Lime.py

---
+++ Lime.py
@@ -0,0 +1,4 @@
+import webbrowser, time
+print('Lime is not open source (CHECK FAQ ON THE README) if u want to get lime lite please get it from the releases page, opening releses in 5s')
+time.sleep(5)
+webbrowser.open('https://github.com/R3CI/Lime-lite/releases')
