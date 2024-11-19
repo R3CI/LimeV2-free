0fe9245613de29036d95654acdd4885b2a9a4bd6
r3ci <139361332+R3CI@users.noreply.github.com>
2024-11-19 18:34:18 +0100

    fix???

--- start.cmd
+++ start.cmd
@@ -1,3 +1,11 @@
-call env\Scripts\activate
+@echo off
+if not exist venv (
+    echo Creating virtual environment...
+    python -m venv venv
+)
+echo Activating virtual environment...
+call venv\Scripts\activate.bat
+echo Installing dependencies from requirements.txt...
+pip install -r requirements.txt
 py main.py
 pause
